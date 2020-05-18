d0fe7a1e3efc4814042d87161150eed95d171466
Alex Dukhno <alex.dukhno@icloud.com>
2020-05-18 20:26:33 +0300

    connect and execute query from psql

--- tests/sql/mod.rs
+++ tests/sql/mod.rs
@@ -1,4 +1,10 @@
+use postgres::{Client, Config, NoTls};
+use std::io;
+use std::str::FromStr;
+
 #[test]
-fn integration_test() {
-    assert!(true, "it works!!!")
+#[ignore] // use psql for integration testing
+fn create_simple_database() {
+    /// psql use sslmode `require`
+    let mut client = Client::connect("host=localhost user=postgres password=pass", NoTls).unwrap();
 }
