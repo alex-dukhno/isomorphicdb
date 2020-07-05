fb702c23f831c0599f17fd30112ad44bf0c74e15
Aleks Anatoliyovich <aleks0010v@gmail.com>
2020-07-05 10:57:17 +0300

    SQL types tests improvements

--- tests/functional/queries_with_sql_types.py
+++ tests/functional/queries_with_sql_types.py
@@ -18,57 +18,63 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def create_cursor(request):
-    try:
-        conn = pg.connect(host="localhost", password="check_this_out", database="postgres")
-        cur = conn.cursor()
+    cur = None
+    conn = None
 
-        def close_all():
-            conn.close()
-            cur.close()
+    conn = pg.connect(host="localhost", password="check_this_out", database="postgres")
+    assert conn is not None
 
-        request.addfinalizer(close_all)
-    except Exception as e:
-        assert False, str(e)
+    cur = conn.cursor()
+    assert cur is not None
+
+    def close_all():
+        cur.close()
+        conn.close()
+
+    request.addfinalizer(close_all)
+
     return cur
 
 
-def test_integer_types(create_cursor):
+@pytest.fixture(scope='function')
+def create_drop_test_schema_fixture(request, create_cursor):
     cur = create_cursor
-    try:
-        cur.execute('create schema schema_name;')
-        cur.execute('create table schema_name.table_name(si_col smallint, i_col integer, bi_col bigint);')
+    cur.execute('create schema schema_name;')
 
-        cur.execute('insert into schema_name.table_name values (%d, %d, %d);' % (-32768, -2147483648, -9223372036854775808))
-        cur.execute('insert into schema_name.table_name values (%d, %d, %d);' % (32767, 2147483647, 9223372036854775807))
+    def close_all():
+        cur.execute("drop schema schema_name cascade;")
 
-        cur.execute('select * from schema_name.table_name;')
-        r = cur.fetchmany(2)
-        assert r == [(-32768, -2147483648, -9223372036854775808,), (32767, 2147483647, 9223372036854775807,)]
-    finally:
-        cur.execute('drop table schema_name.table_name;')
-        cur.execute('drop schema schema_name;')
+    request.addfinalizer(close_all)
+    return cur
 
 
-def test_character_types(create_cursor):
-    cur = create_cursor
-    try:
-        cur.execute('create schema schema_name;')
-        cur.execute(
-            'create table schema_name.table_name(\
-                col_no_len_chars char,\
-                col_with_len_chars char(10),\
-                col_var_char_smallest varchar(1),\
-                col_var_char_large    varchar(20)\
-                );')
+def test_integer_types(create_drop_test_schema_fixture):
+    cur = create_drop_test_schema_fixture
+    cur.execute('create table schema_name.table_name(si_col smallint, i_col integer, bi_col bigint);')
 
-        cur.execute(
-            'insert into schema_name.table_name values (\'%s\', \'%s\', \'%s\', \'%s\');' % ('c', '1234567890', 'c', '12345678901234567890'))
-        cur.execute(
-            'insert into schema_name.table_name values (\'%s\', \'%s\', \'%s\', \'%s\');' % ('1', '1234567   ', 'c', '1234567890'))
+    cur.execute('insert into schema_name.table_name values (%d, %d, %d);' % (-32768, -2147483648, -9223372036854775808))
+    cur.execute('insert into schema_name.table_name values (%d, %d, %d);' % (32767, 2147483647, 9223372036854775807))
 
-        cur.execute('select * from schema_name.table_name;')
-        r = cur.fetchmany(2)
-        assert r == [('c', '1234567890', 'c', '12345678901234567890',), ('1', '1234567', 'c', '1234567890',)]
-    finally:
-        cur.execute('drop table schema_name.table_name;')
-        cur.execute('drop schema schema_name;')
+    cur.execute('select * from schema_name.table_name;')
+    r = cur.fetchmany(2)
+    assert r == [(-32768, -2147483648, -9223372036854775808,), (32767, 2147483647, 9223372036854775807,)]
+
+
+def test_character_types(create_drop_test_schema_fixture):
+    cur = create_drop_test_schema_fixture
+    cur.execute(
+        'create table schema_name.table_name(\
+            col_no_len_chars char,\
+            col_with_len_chars char(10),\
+            col_var_char_smallest varchar(1),\
+            col_var_char_large    varchar(20)\
+            );')
+
+    cur.execute(
+        'insert into schema_name.table_name values (\'%s\', \'%s\', \'%s\', \'%s\');' % ('c', '1234567890', 'c', '12345678901234567890'))
+    cur.execute(
+        'insert into schema_name.table_name values (\'%s\', \'%s\', \'%s\', \'%s\');' % ('1', '1234567   ', 'c', '1234567890'))
+
+    cur.execute('select * from schema_name.table_name;')
+    r = cur.fetchmany(2)
+    assert r == [('c', '1234567890', 'c', '12345678901234567890',), ('1', '1234567', 'c', '1234567890',)]
