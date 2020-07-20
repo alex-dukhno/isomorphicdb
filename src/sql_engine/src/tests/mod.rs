31b66d092c3c3c458649a15aa9f5ac115599f073
Andrew Bregger <adb3649@tamu.edu>
2020-07-19 19:03:42 -0500

    Fixed cargo clippy

--- src/sql_engine/src/tests/mod.rs
+++ src/sql_engine/src/tests/mod.rs
@@ -32,9 +32,8 @@ mod update;
 use super::*;
 use crate::QueryExecutor;
 use in_memory_backend_storage::InMemoryStorage;
-use sql_types::SqlType;
 use std::sync::{Arc, Mutex};
-use storage::{frontend::FrontendStorage, ColumnDefinition};
+use storage::frontend::FrontendStorage;
 
 fn in_memory_storage() -> Arc<Mutex<FrontendStorage<InMemoryStorage>>> {
     Arc::new(Mutex::new(FrontendStorage::new(InMemoryStorage::default()).unwrap()))
@@ -55,8 +54,4 @@ fn sql_engine_with_schema(mut sql_engine: InMemorySqlEngine) -> InMemorySqlEngin
         .expect("schema created");
 
     sql_engine
-}
-
-fn column_definition(name: &'static str, sql_type: SqlType) -> ColumnDefinition {
-    ColumnDefinition::new(name, sql_type)
 }
