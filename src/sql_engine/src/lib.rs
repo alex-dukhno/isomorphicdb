ffef78ef9c169c43a518432836754ffb7ea8aa5e
Alex Dukhno <alex.dukhno@icloud.com>
2020-08-29 20:03:09 +0300

    remove dead code

--- src/sql_engine/src/lib.rs
+++ src/sql_engine/src/lib.rs
@@ -48,58 +48,6 @@ mod session;
 
 pub type Projection = (Vec<ColumnDefinition>, Vec<Vec<String>>);
 
-#[derive(Debug, Clone)]
-pub struct TableDefinition {
-    schema_name: String,
-    table_name: String,
-    column_data: Vec<ColumnDefinition>,
-}
-
-impl TableDefinition {
-    pub fn new(schema_name: &str, table_name: &str, column_data: Vec<ColumnDefinition>) -> Self {
-        Self {
-            schema_name: schema_name.to_owned(),
-            table_name: table_name.to_owned(),
-            column_data,
-        }
-    }
-
-    pub fn column_len(&self) -> usize {
-        self.column_data.len()
-    }
-
-    pub fn column_type(&self, column_idx: usize) -> SqlType {
-        if let Some(column) = self.column_data.get(column_idx) {
-            column.sql_type
-        } else {
-            panic!("attempting to access type of invalid column index")
-        }
-    }
-
-    pub fn column_type_by_name(&self, name: &str) -> Option<SqlType> {
-        self.column_data
-            .iter()
-            .find(|column| column.name == name)
-            .map(|column| column.sql_type)
-    }
-
-    pub fn column_data(&self) -> &[ColumnDefinition] {
-        self.column_data.as_slice()
-    }
-
-    pub fn scheme(&self) -> &str {
-        self.schema_name.as_str()
-    }
-
-    pub fn table(&self) -> &str {
-        self.table_name.as_str()
-    }
-
-    pub fn full_name(&self) -> String {
-        format!("{}.{}", self.schema_name, self.table_name)
-    }
-}
-
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct ColumnDefinition {
     name: String,
