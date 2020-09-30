2dedaf75ca57bd5e2ec9c72521abc4fd339ce2e9
Alex Dukhno <5074607+alex-dukhno@users.noreply.github.com>
2020-09-30 21:48:52 +0300

    single table dataflow based on iterator (#348)

--- src/query_executor/src/dml/select.rs
+++ src/query_executor/src/dml/select.rs
@@ -12,11 +12,89 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use binary::Binary;
+use data_manager::{DataManager, MetadataView, ReadCursor};
+use plan::{SelectInput, TableId};
+use protocol::{messages::ColumnMetadata, results::QueryEvent, Sender};
+use sql_model::Id;
 use std::sync::Arc;
 
-use data_manager::{DataManager, MetadataView};
-use plan::SelectInput;
-use protocol::{messages::ColumnMetadata, results::QueryEvent, Sender};
+struct Source {
+    table_id: TableId,
+    cursor: Option<ReadCursor>,
+    data_manager: Arc<DataManager>,
+}
+
+impl Source {
+    fn new(table_id: TableId, data_manager: Arc<DataManager>) -> Source {
+        Source {
+            table_id,
+            cursor: None,
+            data_manager,
+        }
+    }
+}
+
+impl Iterator for Source {
+    type Item = Binary;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cursor.is_none() {
+            self.cursor = Some(self.data_manager.full_scan(&self.table_id).expect("no errors"));
+        }
+        if let Some(cursor) = self.cursor.as_mut() {
+            cursor
+                .next()
+                .map(Result::unwrap)
+                .map(Result::unwrap)
+                .map(|(_key, values)| values)
+        } else {
+            None
+        }
+    }
+}
+
+struct Projection<I: Iterator<Item = Binary>> {
+    selected_columns: Vec<Id>,
+    input: Option<I>,
+    consumed: usize,
+}
+
+impl<I: Iterator<Item = Binary>> Projection<I> {
+    fn new(selected_columns: Vec<Id>) -> Projection<I> {
+        Projection {
+            selected_columns,
+            input: None,
+            consumed: 0,
+        }
+    }
+
+    fn connect(&mut self, input: I) {
+        self.input = Some(input)
+    }
+}
+
+impl<'p, I: Iterator<Item = Binary>> Iterator for &'p mut Projection<I> {
+    type Item = Vec<String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(input) = self.input.as_mut() {
+            if let Some(values) = input.next() {
+                let data = values.unpack();
+                let mut values = vec![];
+                for origin in self.selected_columns.iter() {
+                    values.push(data[*origin as usize].to_string());
+                }
+                self.consumed += 1;
+                Some(values)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
 
 pub(crate) struct SelectCommand {
     select_input: SelectInput,
@@ -37,14 +115,7 @@ impl SelectCommand {
         }
     }
 
-    pub(crate) fn execute(&mut self) {
-        let records = match self.data_manager.full_scan(&self.select_input.table_id) {
-            Err(()) => {
-                log::error!("Error while scanning table {:?}", self.select_input.table_id);
-                return;
-            }
-            Ok(records) => records,
-        };
+    pub(crate) fn execute(self) {
         self.sender
             .send(Ok(QueryEvent::RowDescription(
                 self.data_manager
@@ -55,33 +126,18 @@ impl SelectCommand {
             )))
             .expect("To Send Query Result to Client");
 
-        let mut index = 0;
+        let source = Source::new(self.select_input.table_id, self.data_manager.clone());
+        let mut projection = Projection::new(self.select_input.selected_columns);
+        projection.connect(source);
 
-        records
-            .map(Result::unwrap)
-            .map(Result::unwrap)
-            .map(|(_key, values)| {
-                let row: Vec<String> = values.unpack().into_iter().map(|datum| datum.to_string()).collect();
-
-                let mut values = vec![];
-                for origin in self.select_input.selected_columns.iter() {
-                    for (index, value) in row.iter().enumerate() {
-                        if (index as u64) == *origin {
-                            values.push(value.clone())
-                        }
-                    }
-                }
-                values
-            })
-            .for_each(|value| {
-                self.sender
-                    .send(Ok(QueryEvent::DataRow(value)))
-                    .expect("To Send Query Result to Client");
-                index += 1;
-            });
+        for tuple in &mut projection {
+            self.sender
+                .send(Ok(QueryEvent::DataRow(tuple)))
+                .expect("To Send Query Result to Client");
+        }
 
         self.sender
-            .send(Ok(QueryEvent::RecordsSelected(index)))
+            .send(Ok(QueryEvent::RecordsSelected(projection.consumed)))
             .expect("To Send Query Result to Client");
     }
 }
