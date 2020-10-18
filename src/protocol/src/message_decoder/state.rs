7e569f8dcb9307e34faecf77655d1409dd132008
Alex Dukhno <5074607+alex-dukhno@users.noreply.github.com>
2020-10-18 21:35:06 +0300

    addition to message decoder state (#390)

--- src/protocol/src/message_decoder/state.rs
+++ src/protocol/src/message_decoder/state.rs
@@ -18,57 +18,57 @@ trait Transform<C> {
     fn transform(self, buf: &mut Cursor) -> Result<C>;
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq)]
 pub(crate) struct Created;
 
-impl Transform<RT> for Created {
-    fn transform(self, _buf: &mut Cursor) -> Result<RT> {
-        Ok(RT)
+impl<'c> Transform<RequestingTag> for &'c Created {
+    fn transform(self, _buf: &mut Cursor) -> Result<RequestingTag> {
+        Ok(RequestingTag)
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub(crate) struct RT;
+#[derive(Debug, PartialEq)]
+pub(crate) struct RequestingTag;
 
-impl Transform<Tag> for RT {
+impl<'rt> Transform<Tag> for &'rt RequestingTag {
     fn transform(self, buf: &mut Cursor) -> Result<Tag> {
         Ok(Tag(buf.read_byte()?))
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq)]
 pub(crate) struct Tag(pub(crate) u8);
 
-impl Transform<W> for Tag {
-    fn transform(self, _buf: &mut Cursor) -> Result<W> {
-        Ok(W)
+impl<'t> Transform<WaitingForPayload> for &'t Tag {
+    fn transform(self, _buf: &mut Cursor) -> Result<WaitingForPayload> {
+        Ok(WaitingForPayload)
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub(crate) struct W;
+#[derive(Debug, PartialEq)]
+pub(crate) struct WaitingForPayload;
 
-impl Transform<Payload> for W {
+impl<'w> Transform<Payload> for &'w WaitingForPayload {
     fn transform(self, buf: &mut Cursor) -> Result<Payload> {
         Ok(Payload(Vec::from(&*buf)))
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq)]
 pub(crate) struct Payload(pub(crate) Vec<u8>);
 
-impl Transform<Created> for Payload {
+impl<'p> Transform<Created> for &'p Payload {
     fn transform(self, _buf: &mut Cursor) -> Result<Created> {
         Ok(Created)
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq)]
 pub(crate) enum State {
     Created(Created),
-    RequestingTag(RT),
+    RequestingTag(RequestingTag),
     Tag(Tag),
-    WaitingForPayload(W),
+    WaitingForPayload(WaitingForPayload),
     Payload(Payload),
 }
 
@@ -77,14 +77,14 @@ impl State {
         State::Created(Created)
     }
 
-    pub(crate) fn try_step(self, buf: &[u8]) -> Result<State> {
+    pub(crate) fn try_step(self, buf: &[u8]) -> Result<(State, State)> {
         let mut cursor = Cursor::from(buf);
-        match self {
-            State::Created(created) => Ok(State::RequestingTag(created.transform(&mut cursor)?)),
-            State::RequestingTag(rt) => Ok(State::Tag(rt.transform(&mut cursor)?)),
-            State::Tag(tag) => Ok(State::WaitingForPayload(tag.transform(&mut cursor)?)),
-            State::WaitingForPayload(w) => Ok(State::Payload(w.transform(&mut cursor)?)),
-            State::Payload(decoded) => Ok(State::Created(decoded.transform(&mut cursor)?)),
+        match &self {
+            State::Created(created) => Ok((State::RequestingTag(created.transform(&mut cursor)?), self)),
+            State::RequestingTag(rt) => Ok((State::Tag(rt.transform(&mut cursor)?), self)),
+            State::Tag(tag) => Ok((State::WaitingForPayload(tag.transform(&mut cursor)?), self)),
+            State::WaitingForPayload(w) => Ok((State::Payload(w.transform(&mut cursor)?), self)),
+            State::Payload(decoded) => Ok((State::Created(decoded.transform(&mut cursor)?), self)),
         }
     }
 }
@@ -105,51 +105,66 @@ mod tests {
     fn requesting_tag() {
         let state = State::new();
 
-        assert_eq!(state.try_step(&[]), Ok(State::RequestingTag(RT)))
+        assert_eq!(
+            state.try_step(&[]),
+            Ok((State::RequestingTag(RequestingTag), State::new()))
+        );
     }
 
     #[test]
     fn parse_tag() {
-        let mut state = State::new();
+        let state = State::new();
 
-        state = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
 
-        assert_eq!(state.try_step(&[QUERY]), Ok(State::Tag(Tag(QUERY))))
+        assert_eq!(
+            state.try_step(&[QUERY]),
+            Ok((State::Tag(Tag(QUERY)), State::RequestingTag(RequestingTag)))
+        );
     }
 
     #[test]
     fn decoding_body() {
-        let mut state = State::new();
+        let state = State::new();
 
-        state = state.try_step(&[]).expect("proceed to the next step");
-        state = state.try_step(&[QUERY]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[QUERY]).expect("proceed to the next step");
 
-        assert_eq!(state.try_step(&[]), Ok(State::WaitingForPayload(W)));
+        assert_eq!(
+            state.try_step(&[]),
+            Ok((State::WaitingForPayload(WaitingForPayload), State::Tag(Tag(QUERY))))
+        );
     }
 
     #[test]
     fn read_body() {
-        let mut state = State::new();
+        let state = State::new();
 
-        state = state.try_step(&[]).expect("proceed to the next step");
-        state = state.try_step(&[QUERY]).expect("proceed to the next step");
-        state = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[QUERY]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
 
         assert_eq!(
             state.try_step(QUERY_STRING),
-            Ok(State::Payload(Payload(QUERY_STRING.to_vec())))
+            Ok((
+                State::Payload(Payload(QUERY_STRING.to_vec())),
+                State::WaitingForPayload(WaitingForPayload)
+            ))
         );
     }
 
     #[test]
     fn full_cycle() {
-        let mut state = State::new();
+        let state = State::new();
 
-        state = state.try_step(&[]).expect("proceed to the next step");
-        state = state.try_step(&[QUERY]).expect("proceed to the next step");
-        state = state.try_step(&[]).expect("proceed to the next step");
-        state = state.try_step(QUERY_STRING).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[QUERY]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(&[]).expect("proceed to the next step");
+        let (state, _prev) = state.try_step(QUERY_STRING).expect("proceed to the next step");
 
-        assert_eq!(state.try_step(&[]), Ok(State::Created(Created)));
+        assert_eq!(
+            state.try_step(&[]),
+            Ok((State::Created(Created), State::Payload(Payload(QUERY_STRING.to_vec()))))
+        );
     }
 }
