4cf9c7a0d656c944fcb0df13d0e05aee9d0e8dfe
Alex Dukhno <alex.dukhno@icloud.com>
2021-02-15 21:41:52 +0200

    remove licenses and fix local code coverage

--- etc/local/code_coverage.sh
+++ etc/local/code_coverage.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-rustup toolchain install nightly
-
-export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
-export RUSTDOCFLAGS="-Cpanic=abort"
-
-cargo clean
-cargo +nightly build
-cargo +nightly test --all --lib
-
-grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
+docker run --security-opt seccomp=unconfined -v "${PWD}:/volume" xd009642/tarpaulin sh -c "cargo tarpaulin --ignore-tests -o Html --output-dir ./target/debug/coverage/"
